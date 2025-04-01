a2c88f5b06d60d3500192a5c9ccbeb3c7c0f3e2a
Yakubu Abdulhameed <yakubuabdulhameedugbede@gmail.com>
2025-04-01 06:46:16 +0100

    wer

--- Framework/Screens/Profile.js
+++ Framework/Screens/Profile.js
@@ -1,11 +1,36 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, Image } from 'react-native'
 import React from 'react'
 
 export function Profile() {
     return (
-        <View>
+        <safeAreaView style={styles.container}>
             <Text>Profile</Text>
-        </View>
+            <View style={styles.headerContainer}>
+                <Image
+                    source={require("../../assets/Xskool-v-gold.png")}
+                    style={styles.logo}
+                />
+                <View>
+                    <Text style={{ fontFamily: Theme.fonts.text600, fontSize: 18 }}>John Wick</Text>
+                    <Text style={styles.welcomeText}>Learn, Grow, Explore!</Text>
+                </View>
+            </View>
+            <View style={{ marginVertical: 10, paddingHorizontal: 20 }}>
+                <Carousel
+                    loop
+                    width={screenWidth - 40}
+                    height={170}
+                    autoPlay={true}
+                    data={carouselLinks}
+                    style={{ borderRadius: 10 }}
+                    scrollAnimationDuration={2000}
+                    renderItem={({ index }) => (
+                        <Image style={{ width: '100%', height: 170, borderRadius: 10, }} source={{ uri: carouselLinks[index] }} defaultSource={require("../../assets/slide4.png")} />
+                    )}
+                />
+
+            </View>
+        </safeAreaView>
     )
 }
 
